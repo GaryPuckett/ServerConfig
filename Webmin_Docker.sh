d6c7e570d7bacf4c5b9e9869c38d780ea83f2713
GaryPuckett <gparker@garrettspage.com>
2025-02-22 01:11:33 -0500

    Update Webmin_Docker.sh

--- Webmin_Docker.sh
+++ Webmin_Docker.sh
@@ -19,7 +19,7 @@ SERVER_IP=$(ip -4 route get 1.1.1.1 | awk '{print $7; exit}')
 SERVER_IPV6=$(ip -6 route get 2001:4860:4860::8888 2>/dev/null | awk '{print $7; exit}')
 
 ## 0. Captive Portion
-echo "Webmin Docker v1.16"
+echo "Webmin Docker v1.17"
 echo "IPv4 address: $SERVER_IP"
 echo "IPv6 address: $SERVER_IPV6"
 echo "Hostname: $(hostname)"
