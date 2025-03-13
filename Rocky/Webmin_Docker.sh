ca125387690ef0be3bd9f177586fdbf0321fbc39
GaryPuckett <gparker@garrettspage.com>
2025-03-13 04:00:36 -0400

    Update Webmin_Docker.sh

--- Rocky/Webmin_Docker.sh
+++ Rocky/Webmin_Docker.sh
@@ -37,7 +37,7 @@ SERVER_IP=$(ip -4 route get 1.1.1.1 | awk '{print $7; exit}')
 SERVER_IPV6=$(ip -6 route get 2001:4860:4860::8888 2>/dev/null | awk '{print $7; exit}')
 
 ## 0. Introductory Output
-echo "Ubuntu Webmin Docker v1.04"
+echo "Ubuntu Webmin Docker v1.05"
 echo "IPv4 address: $SERVER_IP"
 echo "IPv6 address: $SERVER_IPV6"
 echo "Hostname: $(hostname)"
